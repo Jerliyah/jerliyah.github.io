462698ecb130850b4be1d5bfa37283bdb0a5e173
Jerliyah <jerliyahcraig@gmail.com>
2017-11-07 14:26:14 -0600

    new direction: simple mobile first

---
+++ scripts/action.js
@@ -0,0 +1,12 @@
+/* TODO:
+- Track scrolling
+- Track panels: class "before" and class "after" based on index value
+
+- current panel gets a class (css to make it glow)
+
+- Change 3D transform of panels based on scrolling value
+
+- Transforming sound
+- click into place sound
+
+*/
