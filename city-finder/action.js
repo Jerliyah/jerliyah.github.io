e7d9ab5d57fd1ddf948f08c302789d69e81af7e7
Jerliyah <jerliyahcraig@gmail.com>
2018-01-09 16:48:38 -0600

    intersting styling :D

--- city-finder/action.js
+++ city-finder/action.js
@@ -1,9 +1,14 @@
 // ----------- Get elements -------------
+var html = document.querySelector('html')
 var search_box = document.querySelector('input#search');
 var panel_ctn = document.querySelector('ul.panels-ctn');
 
+
+
 // ----------- Set elements -------------
 search_box.value = '';
+
+
 
 // ------------- Get data ---------------
 const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
@@ -13,6 +18,7 @@ const cities = [];
 fetch( endpoint )
 .then( stuff => stuff.json() )
 .then( data => cities.push(...data) )
+
 
 
 // ------------- Show data ---------------
@@ -53,7 +59,31 @@ function display_matches() {
         
 
     panel_ctn.innerHTML = panels;
+    fancy_style()
 }
 
 search_box.addEventListener('keyup', display_matches)
+
+
+
+// -------------- Style it up ------------------------
+function random_color() {
+    var characters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += characters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function fancy_style() {
+    search_box.style.width = panel_ctn.offsetWidth + "px";
+    html.style.setProperty('--color', random_color());
+}
+
+
+// --------------- Redirection -----------------
+
+// TODO: when clicking on a city, go to informational page
 
